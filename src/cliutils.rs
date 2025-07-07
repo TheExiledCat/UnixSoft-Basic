ab40bd84aab179a967a7ace2eb264d59686fa490
armando-codecafe <armando.roerdinkveldboom@code-cafe.nl>
2025-07-07 22:47:35 +0200

    compiler

--- src/cliutils.rs
+++ src/cliutils.rs
@@ -1,4 +1,55 @@
-use std::{env, fs, path::PathBuf};
+use std::{env, error::Error, fs, path::PathBuf};
+
+use pico_args::Arguments;
+#[derive(Debug)]
+pub enum Command {
+    Init { working_dir: String },
+    Build { entry: String },
+    Run { entry: String },
+    Help,
+}
+pub fn show_help() {
+    println!("usbasic - UnixSoft BASIC compiler and cli tool");
+}
+impl Command {
+    pub fn new(args: Arguments) -> Result<Self, ()> {
+        let mut args = args;
+        match args.subcommand().unwrap() {
+            Some(arg) => {
+                return Ok(match arg.as_str() {
+                    "init" => Command::Init {
+                        working_dir: args
+                            .opt_free_from_str()
+                            .unwrap()
+                            .unwrap_or_else(|| String::from("./")),
+                    },
+                    _ => Command::Help,
+                });
+            }
+            None => {
+                show_help();
+                return Err(());
+            }
+        };
+    }
+    pub fn run(&self) -> Result<(), u8> {
+        match self {
+            Command::Init { working_dir } => generate_default_project(PathBuf::from(working_dir)),
+            Command::Build { entry } => todo!(),
+            Command::Run { entry } => todo!(),
+            Command::Help => todo!(),
+        }
+        return Ok(());
+    }
+}
+pub trait HelpDisplay {
+    fn display_help(&self);
+}
+impl HelpDisplay for Command {
+    fn display_help(&self) {
+        todo!()
+    }
+}
 
 fn relative_to_absolute(path: PathBuf) -> PathBuf {
     if path.is_absolute() {
@@ -12,7 +63,7 @@ fn create_project_directory(dir: PathBuf) {
     fs::create_dir(&dir.join("build")).unwrap();
     fs::create_dir(&dir.join("obj")).unwrap();
     fs::create_dir(&dir.join("src")).unwrap();
-    fs::write(&dir.join("src/main.usb"), "PRINT \"Hello, World\"").unwrap();
+    fs::write(&dir.join("src/main.usb"), "10 PRINT \"Hello, World\"").unwrap();
 }
 pub fn generate_default_project(dir: PathBuf) {
     let dir = relative_to_absolute(dir);
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,9 @@
+use cliutils::Command;
 use pico_args::Arguments;
 
 mod cliutils;
+mod usbcompiler;
 
-#[derive(Debug)]
-enum Command {
-    Init { working_dir: String },
-    Build { entry: String },
-    Run { entry: String },
-    Help,
-}
-fn show_help() {
-    println!("usbasic - UnixSoft BASIC compiler and cli tool");
-}
-impl Command {
-    fn new(args: Arguments) -> Result<Self, ()> {
-        let mut args = args;
-        match args.subcommand().unwrap() {
-            Some(arg) => {
-                return Ok(match arg.as_str() {
-                    "init" => Command::Init {
-                        working_dir: args
-                            .opt_free_from_str()
-                            .unwrap()
-                            .unwrap_or_else(|| String::from("./")),
-                    },
-                    _ => Command::Help,
-                });
-            }
-            None => {
-                show_help();
-                return Err(());
-            }
-        };
-    }
-}
-trait HelpDisplay {
-    fn display_help(&self);
-}
-impl HelpDisplay for Command {
-    fn display_help(&self) {
-        todo!()
-    }
-}
 fn main() {
     let args = Arguments::from_env();
     if let Ok(command) = Command::new(args) {
