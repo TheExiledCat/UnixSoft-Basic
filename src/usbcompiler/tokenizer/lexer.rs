be64a48a43dd6c3ddacfd0f80f49ea7d6cba9cee
TheExiledCat <kazekage2002@hotmail.com>
2025-07-08 16:29:41 +0200

    lexer helper functions start

--- src/usbcompiler/tokenizer/lexer.rs
+++ src/usbcompiler/tokenizer/lexer.rs
@@ -13,7 +13,17 @@ pub enum Token {
     ParenClose,
     EOF,
 }
-
+#[repr(u8)]
+#[derive(PartialEq, Eq)]
+enum CharType {
+    None = 0,
+    Numeric,
+    Alphabetic,
+    OperatorSymbol,
+    Delimiter,
+    Whitespace,
+    Quote,
+}
 struct Lexer {
     input: Vec<char>,
     position: usize,
@@ -21,6 +31,7 @@ struct Lexer {
 }
 
 impl Lexer {
+    ///creates a new USB Lexer given a string to tokenize and an optional character start position
     pub fn new(script: String, start_position: usize) -> Self {
         let mut lexer = Lexer {
             input: script.chars().collect(),
@@ -31,20 +42,93 @@ impl Lexer {
         lexer.advance();
         return lexer;
     }
+    ///peek into the character stream by **count** indices, returns a [`Some`] containing the peeked char if the offset given was not out of bounds. else returns [`None`]
     fn peek(&mut self, count: usize) -> Option<char> {
-        todo!();
+        return self.input.get(self.position + count).copied();
     }
-    fn peek_word(&mut self) -> String {
-        todo!();
+
+    ///peek at the next word in the stream, returning it as a copied [`String`] the position of the lexer has to be on a non white space character or this will always return [`None`]
+    fn peek_word(&mut self) -> Option<(CharType, String)> {
+        let current_char = &self.current_char;
+
+        if let None = current_char {
+            return None;
+        }
+
+        let mut c = current_char.unwrap();
+
+        if c.is_whitespace() {
+            return None;
+        }
+        let mut peek_pos = 0;
+        let mut last_char_type: CharType = CharType::None;
+        let mut total = String::new();
+        while let Some(c) = self.peek(peek_pos) {
+            let new_char_type = Lexer::get_char_type(&c);
+            if last_char_type != CharType::None {
+                if last_char_type != new_char_type
+                    && (last_char_type != CharType::Alphabetic
+                        && new_char_type == CharType::Numeric)
+                {
+                    break;
+                }
+            }
+
+            last_char_type = new_char_type;
+            total.push(c);
+            peek_pos += 1;
+        }
+
+        return Some((last_char_type, total));
     }
+
+    ///peek at the next none whitespace character in the stream, returning a [`Some`] containing a tuple with the offsett to the next non whitespace char and the char at that position
+    fn peek_next_non_whitespace(&mut self) -> Option<(usize, char)> {
+        let mut peek_pos = 0;
+        while let Some(c) = self.peek(peek_pos) {
+            if !&c.is_whitespace() {
+                return Some((peek_pos, c.clone()));
+            }
+            peek_pos += 1;
+        }
+        return None;
+    }
+
     fn consume(&mut self, count: usize) -> String {
-        todo!();
+        let mut total = String::new();
+        for i in 0..count {
+            if let Some(c) = self.current_char {
+                total.push(c);
+                self.advance();
+            } else {
+                break;
+            }
+        }
+
+        return total;
     }
     fn consume_whitespace(&mut self) {
-        todo!();
+        while let Some(c) = self.current_char {
+            if !c.is_whitespace() {
+                break;
+            }
+            self.advance();
+        }
     }
-    fn consume_word(&mut self) -> String {
-        todo!();
+    /// consumes the next word in the stream, skipping any white space first and returning the consumed word.
+    ///
+    /// A word is any of the following:
+    /// - a string of alphabetics containing 0 or more numerics positioned after the first character.
+    /// - an operator or a compound operator e.g. <= or =
+    /// - a delimiter
+    /// - a string literal including both the start and end quotes
+    fn consume_word(&mut self) -> Option<(CharType, String)> {
+        let word = self.peek_word();
+        if let Some(w) = word {
+            self.skip(w.1.len());
+            return Some(w);
+        }
+        return None;
     }
     fn advance(&mut self) {
         if self.position < self.input.len() {
@@ -54,8 +138,39 @@ impl Lexer {
             self.current_char = None;
         }
     }
+    /// advance the through the stream by `count` steps
+    fn skip(&mut self, count: usize) {
+        let mut index = 0;
+        while let Some(_) = self.current_char {
+            if index >= count {
+                break;
+            }
+            self.advance();
+            index += 1;
+        }
+    }
     pub fn next_token(&mut self) -> Token {
         todo!();
+    }
+
+    fn get_char_type(character: &char) -> CharType {
+        return if character.is_whitespace() {
+            CharType::Whitespace
+        } else if character.is_alphabetic() {
+            CharType::Alphabetic
+        } else if character.is_numeric() {
+            CharType::Numeric
+        } else if APPLESOFT_OPERATORS.contains(&character.to_string().as_str())
+            || UNIXSOFT_OPERATORS.contains(&character.to_string().as_str())
+        {
+            CharType::OperatorSymbol
+        } else if UNIXSOFT_DELIMITERS.contains(&character.to_string().as_str()) {
+            CharType::Delimiter
+        } else if character.clone() == '\"' {
+            CharType::Quote
+        } else {
+            panic!("Invalid symbol detected");
+        };
     }
 }
 
@@ -78,3 +193,4 @@ pub const APPLESOFT_OPERATORS: &'static [&'static str] =
 pub const UNIXSOFT_KEYWORDS: &'static [&'static str] = &[];
 pub const UNIXSOFT_FUNCTIONS: &'static [&'static str] = &[];
 pub const UNIXSOFT_OPERATORS: &'static [&'static str] = &[">=", "<=", "!="];
+pub const UNIXSOFT_DELIMITERS: &'static [&'static str] = &["(", ")", "[", "]", ",", ":"];
